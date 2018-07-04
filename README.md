a04e66514fa3b392fc8a98bcbe0cb9cbfff514cc
guylaw <glaw@usa.com>
2018-07-04 11:36:06 -0600

    Remove solution.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # date_range
 
 Developer Exercise
+
+
--- dates.go
+++ dates.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"time"
-	"errors"
 )
 
 func main() {
@@ -16,23 +15,6 @@ func main() {
 func findOverlap(dateRange1 DateRange, dateRange2 DateRange) (DateRange, error) {
 	var overlap DateRange
 	var err error
-
-	if dateRange1.begin.After(dateRange2.begin) {
-		overlap.begin = dateRange1.begin
-
-	} else {
-		overlap.begin = dateRange2.begin
-	}
-
-	if dateRange1.end.Before(dateRange2.end) {
-		overlap.end = dateRange1.end
-	} else {
-		overlap.end = dateRange2.end
-	}
-
-	if overlap.end.Before(overlap.begin) || overlap.end.Equal(overlap.begin) {
-		err = errors.New("No Overlap")
-	}
 
 	return overlap, err
 }
