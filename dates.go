07c71c2318f7927af9927c90103adc7ee437bc44
guylaw <glaw@usa.com>
2018-07-04 10:19:40 -0600

    Remove extra parenthesis

--- dates.go
+++ dates.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"time"
+	"errors"
 )
 
 func main() {
@@ -16,10 +17,24 @@ func findOverlap(dateRange1 DateRange, dateRange2 DateRange) (DateRange, error) 
 	var overlap DateRange
 	var err error
 
+	if dateRange1.begin.After(dateRange2.begin) {
+		overlap.begin = dateRange1.begin
 
+	} else {
+		overlap.begin = dateRange2.begin
+	}
+
+	if dateRange1.end.Before(dateRange2.end) {
+		overlap.end = dateRange1.end
+	} else {
+		overlap.end = dateRange2.end
+	}
+
+	if overlap.end.Before(overlap.begin) || overlap.end.Equal(overlap.begin) {
+		err = errors.New("No Overlap")
+	}
 
 	return overlap, err
-
 }
 
 type DateRange struct {
